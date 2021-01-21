d34ccb9b294463baa18db73028e5dbf432e92db4
p1t1ch <belobeev.kirill@gmail.com>
2021-01-21 18:22:03 +0300

    remove dynamic classnames and fix page alignment

--- src/components/Ratings/index.tsx
+++ src/components/Ratings/index.tsx
@@ -8,7 +8,7 @@ function Ratings({ children, className = '', ...props }: RatingsProps) {
   return (
     <ul className={`grid gap-4 ${className}`} {...props}>
       {React.Children.map(children, (child, i) => (
-        <li className={`${i ? `md:ml-${12 * i}` : ''}`}>{child}</li>
+        <li className={`${i === 1 ? 'md:ml-12' : i === 2 ? 'md:ml-24' : ''}`}>{child}</li>
       ))}
     </ul>
   )
--- src/components/Reviews/index.tsx
+++ src/components/Reviews/index.tsx
@@ -8,7 +8,7 @@ function Reviews({ children, className = '', ...props }: ReviewsProps) {
   return (
     <ul className={`grid md:grid-cols-3 gap-4 md:gap-8 ${className}`} {...props}>
       {React.Children.map(children, (child, i) => (
-        <li className={`${i ? `md:mt-${4 * i}` : ''}`}>{child}</li>
+        <li className={`${i === 1 ? 'md:mt-4' : i === 2 ? 'md:mt-8' : ''}`}>{child}</li>
       ))}
     </ul>
   )
